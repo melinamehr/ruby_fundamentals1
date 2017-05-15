56b65183eb04dfb11feaf50f13e7bf2631d1397f
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-05-15 15:22:22 -0400

    exercise 4 completed

--- exercise3.rb
+++ exercise3.rb
@@ -6,3 +6,10 @@ puts "How old are you?"
 age = gets.chomp
 year = age.to_i
 puts "You were born in #{2017 - year}"
+
+
+counter = 3
+until counter == 0
+  puts "counter currently at #{counter}"
+  counter -= 1
+end
---
+++ exercise4.rb
@@ -0,0 +1,11 @@
+(1..100).each do |i|
+  if i%3 == 0 && i%5 == 0
+  puts "Bitmaker"
+  elsif i%3 == 0
+    puts "Bit"
+  elsif i%5 == 0
+    puts "Maker"
+  else
+    puts i
+  end
+end
