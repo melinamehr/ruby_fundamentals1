c0dae96f3b8a55f40f8a92e71185a037419093b2
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-05-15 13:28:18 -0400

    exercise two practice

---
+++ exercise2.rb
@@ -0,0 +1,6 @@
+puts 55.0 * 0.20
+# float numbers seem to require the 0 or else ruby can't compute them
+puts "feelin like a #{5 + 5}"
+puts "there are #{45628 * 7839} hours in a decade"
+(10 < 20 && 30 < 20) || !(10 == 11)
+# true and false or true (10 is equal to 11, not)
