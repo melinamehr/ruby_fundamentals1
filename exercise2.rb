1e21d2939f78785135f0e328402f0e5626dce5f5
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-05-15 13:59:27 -0400

    testing ruby greetings

--- exercise2.rb
+++ exercise2.rb
@@ -4,3 +4,4 @@ puts "feelin like a #{5 + 5}"
 puts "there are #{45628 * 7839} hours in a decade"
 (10 < 20 && 30 < 20) || !(10 == 11)
 # true and false or true (10 is equal to 11, not)
+my_variable = 'my_variable is now this string'
